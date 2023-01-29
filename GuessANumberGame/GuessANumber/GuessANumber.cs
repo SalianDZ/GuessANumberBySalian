4de6347d85d3256fbe1dc3153c214d14fb44573c
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-01-29 22:25:26 +0200

    A nice and simple game if you have nothing to do

---
+++ GuessANumberGame/GuessANumber/GuessANumber.cs
@@ -0,0 +1,30 @@
+﻿Random randomNumber = new Random();
+int computerNumber = randomNumber.Next(1, 101);
+
+while (true)
+{
+    Console.Write("Guess the number (1- 100): ");
+    string playerInput = Console.ReadLine();
+    bool isValid = int.TryParse(playerInput, out int playerNumber);
+
+    if (isValid)
+    {
+        if (playerNumber == computerNumber)
+        {
+            Console.WriteLine("You guessed it!");
+            break;
+        }
+        else if (playerNumber > computerNumber)
+        {
+            Console.WriteLine("Too High!");
+        }
+        else
+        {
+            Console.WriteLine("Too Low!");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid input! Try again.");
+    }
+}
